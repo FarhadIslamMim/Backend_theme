53c3ba98a0a8400ffe856b6f0f42ea217d9be876
Mim <farhad15-11090@diu.edu.bd>
2022-11-20 17:42:57 +0600

    Backend: backend theme page added

--- routes/web.php
+++ routes/web.php
@@ -2,24 +2,23 @@
 
 use Illuminate\Support\Facades\Route;
 
-//Test Route::START
-Route::group(['prefix' => 'command'], function () {
-    Route::get('clear', function () {
-        Artisan::call('cache:clear');
-        Artisan::call('config:clear');
-        Artisan::call('route:clear');
-        Artisan::call('view:clear');
-        dd("All clear!");
-    });
-
-    Route::get('/storage-link', function () {
-        Artisan::call('storage:link');
-        dd("Added!");
-    });
-});
-//Test Route::END
-
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('admin.admin_master');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
